886bdfdab20c9fc54b7d8809ec6163d5ff44d6fc
Mario Carbache <mariocarbache@gmail.com>
2023-07-22 18:53:00 -0400

    pokemon pokedex part 2

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
+const Pokemon = require('./models/Pokemon');
+
+app.get('/pokemon', async (req, res) =>{
+  const pokemon = await Pokemon.findAll();
+  res.json(pokemon);
+});
+
 
 
 app.get("/", (req, res) => {
@@ -44,6 +51,40 @@ app.get("/pokemon/:id", (req, res) => {
     html += `<p>Trainer: ${pokemon.trainer}</p>`;
     html += `<p>Date: ${pokemon.date}</p>`;
     res.send(html);
+  }
+});
+
+app.get("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+
+app.post("/pokemon", async (req, res) => {
+  const newPokemon = await Pokemon.create(req.body);
+  res.json(newPokemon);
+});
+
+app.put("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.update(req.body);
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+
+app.delete("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.destroy();
+    res.status(204).send();
+  } else {
+    res.status(404).send("Pokemon not found");
   }
 });
 
